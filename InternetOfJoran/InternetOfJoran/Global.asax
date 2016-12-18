c87a63aa68e14d924aa115428eded793a9b46d1d
Joran Claessens <11400919@student.pxl.be>
2016-12-18 19:18:03 +0100

    Initialize project. Version 1.0

---
+++ InternetOfJoran/InternetOfJoran/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InternetOfJoran.MvcApplication" Language="C#" %>
---
+++ InternetOfJoran/InternetOfJoran/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternetOfJoran.Startup))]
+namespace InternetOfJoran
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
